f2d2006a8f96fbbb49c1f763f90418234e711762
fantrai <chtotoschemto53@gmail.com>
2023-12-15 14:19:48 +0300

    CreateRepo
    Description

---
+++ WatchFlowers/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WatchFlowers
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WatchFlowers/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WatchFlowers
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
